1e142c8447dcfea896cb5798365ea112ed48a6ce
Md Azharuddin <87271343+Md-Azharuddin02@users.noreply.github.com>
2023-06-08 22:52:30 +0530

    Update Git.java
    Pull command is used here

--- Git.java
+++ Git.java
@@ -1,5 +1,9 @@
 public class Git{
     public static void main(String[] args) {
         System.out.println("This is for the git command");
+        pullGit();
+    }
+    static void pullGit(){
+        System.out.println("This function is used for pull);
     }
 }
